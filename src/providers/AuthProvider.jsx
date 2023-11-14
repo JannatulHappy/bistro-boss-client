496c17f5c7aa0a3bcca1e723696e76c06d1734fd
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-14 13:05:40 +0600

    firebase added

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { createContext } from "react";
+import app from "../firebase/firebase.config";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+  deleteUser,
+} from "firebase/auth";
+import { useEffect } from "react";
+import axios from "axios";
+import { API_BASE_URL } from "../api/api";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const auth = getAuth(app);
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleProvider = new GoogleAuthProvider();
+
+  const googleSignIn = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const updateUser = (userInfo) => {
+    return updateProfile(auth.currentUser, userInfo);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unscubscribe = onAuthStateChanged(auth, (currentUser) => {
+      const userEmail = currentUser?.email || user?.email;
+      const loggedUser = { email: userEmail };
+      fetch("/user/email");
+      setUser(currentUser);
+      console.log("current user", currentUser);
+      setLoading(false);
+      // if user exists then issue a token
+      if (currentUser) {
+        axios
+          .post(`${API_BASE_URL}/jwt`, loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            console.log("token response", res.data);
+          });
+      } else {
+        // remove token after logout
+        axios
+          .post(`${API_BASE_URL}/logout`, loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            console.log(res.data);
+          });
+      }
+    });
+    return () => {
+      unscubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    setUser,
+    logOut,
+    googleSignIn,
+    updateUser,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
