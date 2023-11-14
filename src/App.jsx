dd3765599ac58a4e50c8a56047081ff4b8948e3b
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-14 11:50:16 +0600

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
+import './App.css'
 
 function App() {
-  
 
   return (
     <>
-    <h1 className="bg-red-300"> Bistro Boss project initialized!</h1>
-     
+      
     </>
   )
 }
--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import NavBar from "../pages/Shared/NavBar/NavBar";
 
-import Footer from "../Pages/Shared/Footer/Footer"
-import Navbar from "../Pages/Shared/Navbar/Navbar";
+
 const Main = () => {
-  return (
-    <div>
-      <Navbar></Navbar>
-      <Outlet></Outlet>
-      <Footer></Footer>
-    </div>
-  );
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
 };
 
 export default Main;
