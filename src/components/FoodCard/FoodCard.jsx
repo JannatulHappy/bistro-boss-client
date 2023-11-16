26a5de6f7830d12467007696fc79ad0d9fa09d29
Jannatul Ferdous Happy <86628137+JannatulHappy@users.noreply.github.com>
2023-11-16 09:20:49 +0600

    implemented auth

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,20 +1,54 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
-
-const FoodCard = ({item}) => {
-    const {name, image, price, recipe} = item;
-    return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={image} alt="Shoes" /></figure>
-            <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
-            <div className="card-body flex flex-col items-center">
-                <h2 className="card-title">{name}</h2>
-                <p>{recipe}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
-                </div>
-            </div>
+const FoodCard = ({ item }) => {
+  const { name, image, price, recipe } = item;
+  const { user } = useAuth();
+  const goTo = useNavigate();
+  const handleAddToCart = (food) => {
+    if (user && user.email) {
+      //todo:send cart item to the database
+      console.log("user ace");
+    } else {
+      Swal.fire({
+        title: "you are not logged in",
+        text: "please login to the the the cart",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, login!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // send to the login page
+          goTo("/login");
+        }
+      });
+    }
+  };
+  return (
+    <div className="shadow-xl card w-96 bg-base-100">
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
+      <p className="absolute right-0 px-4 mt-4 mr-4 text-white bg-slate-900">
+        ${price}
+      </p>
+      <div className="flex flex-col items-center card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="justify-end card-actions">
+          <button
+            onClick={() => handleAddToCart(item)}
+            className="mt-4 border-0 border-b-4 border-orange-400 btn btn-outline bg-slate-100"
+          >
+            Add to Cart
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default FoodCard;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,97 +1,61 @@
-import React, { useState } from "react";
-import { createContext } from "react";
-import app from "../firebase/firebase.config";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-  updateProfile,
-  deleteUser,
-} from "firebase/auth";
-import { useEffect } from "react";
-import axios from "axios";
-import { API_BASE_URL } from "../api/api";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
-export const AuthContext = createContext();
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
-  const auth = getAuth(app);
-  const [user, setUser] = useState({});
-  const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  const createUser = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-  };
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
-  const signIn = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
-  };
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
 
-  const googleProvider = new GoogleAuthProvider();
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
 
-  const googleSignIn = () => {
-    return signInWithPopup(auth, googleProvider);
-  };
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        });
+    }
 
-  const updateUser = (userInfo) => {
-    return updateProfile(auth.currentUser, userInfo);
-  };
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('current user', currentUser);
+            setLoading(false);
+        });
+        return () => {
+            return unsubscribe();
+        }
+    }, [])
 
-  const logOut = () => {
-    return signOut(auth);
-  };
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut,
+        updateUserProfile
+    }
 
-  useEffect(() => {
-    const unscubscribe = onAuthStateChanged(auth, (currentUser) => {
-      const userEmail = currentUser?.email || user?.email;
-      const loggedUser = { email: userEmail };
-      fetch("/user/email");
-      setUser(currentUser);
-      console.log("current user", currentUser);
-      setLoading(false);
-      // if user exists then issue a token
-      if (currentUser) {
-        axios
-          .post(`${API_BASE_URL}/jwt`, loggedUser, {
-            withCredentials: true,
-          })
-          .then((res) => {
-            console.log("token response", res.data);
-          });
-      } else {
-        // remove token after logout
-        axios
-          .post(`${API_BASE_URL}/logout`, loggedUser, {
-            withCredentials: true,
-          })
-          .then((res) => {
-            console.log(res.data);
-          });
-      }
-    });
-    return () => {
-      unscubscribe();
-    };
-  }, []);
-
-  const authInfo = {
-    user,
-    loading,
-    createUser,
-    signIn,
-    setUser,
-    logOut,
-    googleSignIn,
-    updateUser,
-  };
-
-  return (
-    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthProvider;
